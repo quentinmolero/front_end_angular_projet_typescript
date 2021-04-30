cf20a906073e07b84fa47fb27fa68691df28cb87
Quentin MOLERO <quentin.molero@estiam.com>
2021-04-30 16:16:11 +0200

    Add fight methods to make pokemon fight

--- model/fight.model.ts
+++ model/fight.model.ts
@@ -1,4 +1,5 @@
 import {Pokemon} from "./pokemon";
+import {Turn} from "./turn.model";
 
 export interface IFightProps {
     firstPokemon: Pokemon;
@@ -19,11 +20,27 @@ export class Fight implements IFightProps{
     }
 
     ifAnyPokemonDead(): boolean {
-        if (this.isPokemonDead(this.firstPokemon)) {
-            return true;
-        }
-        return this.isPokemonDead(this.secondPokemon);
+        return this.isPokemonDead(this.secondPokemon) || this.isPokemonDead(this.firstPokemon);
     }
 
+    processFight() {
+        const firstAttackingPokemon = Turn.findAttackingPokemon(this.firstPokemon, this.secondPokemon);
+        const lastAttackingPokemon = firstAttackingPokemon === this.firstPokemon ? this.secondPokemon : this.firstPokemon;
 
+        Turn.processPokemonAttack(firstAttackingPokemon, lastAttackingPokemon);
+        if (!this.isPokemonDead(lastAttackingPokemon)) {
+            Turn.processPokemonAttack(lastAttackingPokemon, firstAttackingPokemon);
+        }
+    }
+
+    determineWinner(): Pokemon {
+        return this.isPokemonDead(this.firstPokemon) ? this.secondPokemon : this.firstPokemon;
+    }
+
+    startFight(): Pokemon {
+        while (!this.ifAnyPokemonDead()) {
+            this.processFight();
+        }
+        return this.determineWinner();
+    }
 }
